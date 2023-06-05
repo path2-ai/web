45d3fcde8b907fbda602ca04f5c53078cdbd8360
Johannes Hötter <johannes.hoetter@kern.ai>
2023-06-05 23:16:46 +0200

    init from https://github.com/frontendfyi/rebuilding-linear.app

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/jhoetter/repos/web/app/page": [
+      "/Users/jhoetter/repos/web/styles/globals.css"
+    ]
+  },
+  "/Users/jhoetter/repos/web/app/layout.tsx": [
+    "/Users/jhoetter/repos/web/styles/globals.css"
+  ]
+}
--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   experimental: {
-    newNextLinkBehavior: true,
-    scrollRestoration: true,
+    appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
